59f205ac75092c07d540627135921b8f1ac0e3a3
Damir <102433257+Damir747@users.noreply.github.com>
2024-04-25 21:32:42 +0300

    transition

--- js/modules.js
+++ js/modules.js
@@ -4,31 +4,38 @@ const arrQuestionItems = document.querySelectorAll('.questions__item');
 let arrOpenList = [];
 const MAX_COUNT = 3;
 
-// ? Ещё реализовать:
-// - У выпадающих элементов при раскрытии ответа на вопрос через `стрелку` и скрытии его через `стрелку` проигрывается анимация плавного выпадения.
-// - С аккордеонами можно взаимодействовать с помощью мышки и клавиатуры.
-
 function handleOnClick(evt) {
 	const curElement = evt.currentTarget;
 	curElement.classList.toggle(CLOSED_CLASS);
 	curElement.classList.toggle(OPEN_CLASS);
-
-	const question = curElement.querySelector('.questions__answer');
-	console.log(question.offsetHeight);
-	if (curElement.classList.contains(OPEN_CLASS)) {
-		question.style.display = 'block';
-		arrOpenList.push(curElement);
-	} else {
-		question.style.display = 'none';
-		arrOpenList = arrOpenList.filter(el => el !== curElement);
-	}
-	while (arrOpenList.length > MAX_COUNT) {
-		const el = arrOpenList.shift();
-		handleOnClick({ 'currentTarget': el });
+	const questionElement = curElement.querySelector('.questions__answer');
+	if (questionElement) {
+		if (curElement.classList.contains(OPEN_CLASS)) {
+			questionElement.style.maxHeight = '1000px';
+			questionElement.style.marginTop = '29px';
+			arrOpenList.push(curElement);
+		} else {
+			questionElement.style.maxHeight = 0;
+			questionElement.style.marginTop = 0;
+			arrOpenList = arrOpenList.filter(el => el !== curElement);
+		}
+		while (arrOpenList.length > MAX_COUNT) {
+			const el = arrOpenList.shift();
+			handleOnClick({ 'currentTarget': el });
+		}
 	}
 };
 
 arrQuestionItems.forEach((el) => {
 	el.classList.add(CLOSED_CLASS);
 	el.addEventListener('click', handleOnClick);
+	const questionElement = el.querySelector('.questions__answer');
+	if (questionElement) {
+		questionElement.style.display = 'block';
+		questionElement.style.marginTop = 0;
+		questionElement.style.maxHeight = 0;
+		questionElement.style.overflow = 'hidden';
+		questionElement.style.transition = 'max-height 1.5s, height 1.5s, margin-top 1.5s';
+	}
 });
+
